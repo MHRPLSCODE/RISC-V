a6e5d5d5f4703210cc2d90d207766a803d1212d3
MHRPLSCODE <mhr1494@gmail.com>
2026-06-02 12:40:47 +0530

    Update sign extension module with case statement
    Refactor sign extension logic to use a case statement for different instruction types.

--- rtl/signext.v
+++ rtl/signext.v
@@ -2,5 +2,12 @@ module signext(
     input  [31:0] instr,
     output [31:0] immext
 );
-    assign immext = {{20{instr[31]}}, instr[31:20]};
+    always @(*) begin
+        case(immsrc)
+            2'b00: immext = {{20{instr[31]}}, instr[31:20]};                          // I-type
+            2'b01: immext = {{20{instr[31]}}, instr[31:25], instr[11:7]};             // S-type
+            2'b10: immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0}; // B-type
+            default: immext = 32'b0;
+        endcase
+    end
 endmodule
